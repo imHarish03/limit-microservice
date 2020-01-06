b1f0d69e314c55a13a032ab9fa8d307cee7b75a2
imHarish03 <harishhari302@gmail.com>
2020-01-06 23:58:16 +0530

    Adds simple service to read properties from application.properties file

---
+++ src/main/java/lab/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package lab.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lab.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimiteFromConfig() {
+
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+}
--- src/main/java/lab/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/lab/microservices/limitsservice/LimitsServiceApplication.java
@@ -1,11 +1,11 @@
 package lab.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LimitsServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
 	}
